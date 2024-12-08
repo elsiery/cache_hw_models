19cb1fdbadb5769551d36f4098bcb0a5039b4825
Elsie Rezinold Y <130553077+elsiery@users.noreply.github.com>
2024-12-08 19:46:29 +0530

    Update dm_cache.v

--- dm_cache.v
+++ dm_cache.v
@@ -22,3 +22,29 @@ i_mem_read_data,
 i_mem_read_valid,
 o_mem_read_ready
 );
+
+
+
+input i_cpu_valid;
+input i_cpu_rd_wr;
+input [`ADDRESS_WIDTH-1:0] i_cpu_address;
+
+
+output o_cpu_ready;
+input [`WRITE_DATA-1:0] i_cpu_write_data;
+input [`WRITE_STROBE-1:0] i_cpu_write_strobe;
+ouput [`WRITE_DATA-1:0] o_cpu_read_data;
+ouput o_cpu_read_valid;
+input i_cpu_read_ready;
+
+ouput o_mem_valid;
+ouput o_mem_rd_wr;
+ouput [`ADDRESS_WIDTH-1:0] o_mem_address;
+input i_mem_ready;
+ouput [`WRITE_DATA-1:0] o_mem_write_data;
+ouput [`WRITE_STROBE-1:0] o_mem_write_strobe;
+input [`WRITE_DATA-1:0] i_mem_read_data;
+input i_mem_read_valid;
+ouput o_mem_read_ready;
+
+
