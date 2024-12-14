b92c8d70eb07a9bad62ffe54bfcfeb3582ccceda
Elsie Rezinold Y <130553077+elsiery@users.noreply.github.com>
2024-12-14 12:18:55 +0530

    Update tw_associative_test.v

--- tw_associative/tw_associative_test.v
+++ tw_associative/tw_associative_test.v
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2024 Elsie Rezinold Y
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 `include "tw_associative.v"
 
 
@@ -50,9 +74,9 @@ end
 
 initial  
 begin 
-    $readmemh("/home/elsiery/github_codes/cache_models_hw/tw_associative/vmod/block", test_vector);
-    finalresult = $fopen("/home/elsiery/github_codes/cache_models_hw/tw_associative/vmod/final_result.txt");
-    detailedresult = $fopen("/home/elsiery/github_codes/cache_models_hw/tw_associative/vmod/detailed_result.txt");
+    $readmemh("block", test_vector);
+    finalresult = $fopen("final_result.txt");
+    detailedresult = $fopen("detailed_result.txt");
 end
 
 
